462815c40a05d6f60fe6e3071eb9e94d48d3539b
David <s1lv3rstreak@gmail.com>
2020-02-05 09:44:32 -0600

    1

---
+++ primes_arent_real.py
@@ -0,0 +1,21 @@
+def break_primes():
+    prime = int(input("Name one prime number."))
+    print()
+    reverse = 1/prime
+    extrapolation = prime**2
+    
+    print(f"Sorry, that isn't a prime. {prime} is actually comprised of {reverse} * {extrapolation}. This is, in essence, {reverse*extrapolation}.")
+
+def find_derivative():
+    func = input("Enter a function to take the derivative of. Use x.")
+    for i in range(len(func)):
+        if func[i] == 
+
+
+def main():
+    print("what kind of mathematical fun do you want to have?")
+
+if __name__ == "__main__":
+    break_primes()
+    print()
+    
